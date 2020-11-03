bff63565e532c02c7d11417ea681170cacf326e9
aromaljosebaby <65164420+aromaljosebaby@users.noreply.github.com>
2020-11-03 12:45:35 +0530

    Update landing.js

--- landing.js
+++ landing.js
@@ -2,7 +2,7 @@ function pgLoaded()
 
 {
 
-window.setTimeout(function(){window.location.href='http://localhost:63342/js_learning/landing_pg/landing.html?_ijt=69qmjo56e58aoava129l9tahj4';},5000);
+window.setTimeout(function(){window.location.href='https://www.google.co.in/';},5000);
 
 
 }

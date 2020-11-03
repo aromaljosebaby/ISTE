6c39ee10308af0e2f2c4d3ed04f5fcbfa122d0c4
aromaljosebaby <aromaljosebaby@gmail.com>
2020-11-03 11:06:24 +0530

    lol

---
+++ landing.js
@@ -0,0 +1,8 @@
+function pgLoaded()
+
+{
+
+window.setTimeout(function(){window.location.href='http://localhost:63342/js_learning/landing_pg/landing.html?_ijt=69qmjo56e58aoava129l9tahj4';},5000);
+
+
+}

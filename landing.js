2bbda824bc3a2bb724badb4497944f911c4bed65
aromaljosebaby <65164420+aromaljosebaby@users.noreply.github.com>
2020-11-03 18:00:01 +0530

    Update landing.js

--- landing.js
+++ landing.js
@@ -2,7 +2,7 @@ function pgLoaded()
 
 {
 
-window.setTimeout(function(){window.location.href='https://aromaljosebaby.github.io/ISTE/';},5000);
+window.setTimeout(function(){window.location.href='https://www.google.co.in/';},5000);
 
 
 }
